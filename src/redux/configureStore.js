1511eb878ae4324ac0d09869bb62c72c66197fe0
James Ji <jamesji.penfield@protonmail.com>
2021-07-06 12:14:13 -0400

    combined reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,18 @@
-import { createStore } from "redux";
-import { Reducer, initialState } from "./reducer";
+import { createStore, combineReducers } from "redux";
+import { Campsites } from "./reducers/campsites";
+import { Comments } from "./reducers/comments";
+import { Partners } from "./reducers/partners";
+import { Promotions } from "./reducers/promotions";
 
 export const ConfigureStore = () => {
-  const store = createStore(Reducer, initialState);
+  const store = createStore(
+    combineReducers({
+      campsites: Campsites,
+      comments: Comments,
+      partners: Partners,
+      promotions: Promotions,
+    })
+  );
 
   return store;
 };
